5d0e410a996de6a8f957e6fd606f8026a1695d99
sharpiro <dsharpbb09@gmail.com>
2023-11-04 02:17:37 -0400

    refactor to be more clear but longer

--- src/main.c
+++ src/main.c
@@ -7,6 +7,15 @@
 
 extern const uint64_t LOOKUP_TABLE[256];
 
+void process_char(const char character, uint64_t *hex_value) {
+  uint8_t table_index = character + ' ';
+  if ((uint8_t)(character - 'A') >= 0x1a) {
+    table_index = character;
+  }
+  *hex_value =
+      LOOKUP_TABLE[table_index ^ (uint8_t)*hex_value] ^ *hex_value >> 8;
+}
+
 int main() {
   char user_input[BUFFER_SIZE] = {};
   // FILE *file = fopen("test_input.txt", "r");
@@ -15,30 +24,27 @@ int main() {
   uint64_t hex_value = 0xc96c5795d7870f42;
   for (int i = 0; i < BUFFER_SIZE; i++) {
     char current_char = user_input[i];
-    char current_car_resolved = current_char;
-    if (current_car_resolved == '/' || current_car_resolved == '\\') {
-      if (i == 0) {
-        continue;
-      }
-
-      int slash_index = i;
-      while (current_char == '/' || current_char == '\\') {
-        slash_index++;
-        current_char = user_input[slash_index];
-      }
-      i = slash_index - 1;
-      current_car_resolved = '/';
-    }
-
     if (current_char == 0 || current_char == '\r' || current_char == '\n') {
       break;
     }
 
-    uint8_t table_index = current_car_resolved + ' ';
-    if ((uint8_t)(current_car_resolved - 'A') >= 0x1a) {
-      table_index = current_car_resolved;
+    if (current_char == '/' || current_char == '\\') {
+      if (i == 0) {
+        continue;
+      }
+
+      char next_char = user_input[i + 1];
+      while (next_char == '/' || next_char == '\\') {
+        i++;
+        next_char = user_input[i + 1];
+      }
+      if (next_char == 0 || next_char == '\r' || next_char == '\n') {
+        break;
+      }
+      process_char('/', &hex_value);
+    } else {
+      process_char(current_char, &hex_value);
     }
-    hex_value = LOOKUP_TABLE[table_index ^ (uint8_t)hex_value] ^ hex_value >> 8;
   }
 
   uint64_t hex_value_display = hex_value >> 2 | 0x8000000000000000;
